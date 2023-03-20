308ffd82be30043895e9c9e1e56207dc1fa1e7e4
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-03-20 20:45:31 +0900

    bootstrap and templates

--- my_app/views.py
+++ my_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'my_app/index.html')
+
+
+def home(request):
+    return render(request, 'my_app/home.html')
