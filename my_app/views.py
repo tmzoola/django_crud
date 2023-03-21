f107a8a16851e599de4873c1a8c7d4a88c913e75
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-03-21 20:26:03 +0900

    add model and views generated

--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .models import Student
 
 # Create your views here.
 def index(request):
@@ -9,4 +11,14 @@ def home(request):
     return render(request, 'my_app/home.html')
 
 def add(request):
+    
+    if request.method == "POST":
+        student = Student()
+        student.roll = request.POST['roll']
+        student.name = request.POST['name']
+        student.email = request.POST['email']
+        student.address = request.POST['address']
+        student.phone = request.POST['phone']
+        student.save()
+        return redirect('home')
     return render(request, 'my_app/add.html')
