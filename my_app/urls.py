f88fb0a5a4d735901bcf48c91221f2463c5f3909
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-03-21 21:35:32 +0900

    simple crud project completed

--- my_app/urls.py
+++ my_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('', views.home, name='home'),
     path('add/', views.add, name='add'),
+    path('delete/<int:id>', views.delete ),
+    path('update/<int:id>', views.update ),
+    path('update_new/<int:id>', views.update_new ),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -8,7 +8,9 @@ def index(request):
 
 
 def home(request):
-    return render(request, 'my_app/home.html')
+    students = Student.objects.all()
+    
+    return render(request, 'my_app/home.html',{ 'students':students})
 
 def add(request):
     
@@ -22,3 +24,26 @@ def add(request):
         student.save()
         return redirect('home')
     return render(request, 'my_app/add.html')
+
+
+def delete(request,id):
+    student = Student.objects.get(pk=id)
+    student.delete()
+    return redirect('home')
+
+
+def update(request,id):
+    student = Student.objects.get(pk=id)
+
+    return render(request, "my_app/update.html",{'student':student})
+
+def update_new(request,id):
+    student = Student.objects.get(pk=id)
+    student.roll = request.POST['roll']
+    student.name = request.POST['name']
+    student.email = request.POST['email']
+    student.address = request.POST['address']
+    student.phone = request.POST['phone']
+    student.save()
+    
+    return redirect('home')
